a3700b5297e1611e38da51ec0a3817e6ebf238f4
Egor <egor_chek@mail.ru>
2023-06-13 18:28:55 +0400

    создан интерфейс счётчика, отображение самого значения и кнопка увеличения значения на 1. Во ViewController добавлены методы для нажатия и визуального стиля кнопки

--- CounterYandexPracticumSprint3/ViewController.swift
+++ CounterYandexPracticumSprint3/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var counterLable: UILabel!
+    @IBOutlet weak var counterTitleLable: UILabel!
+    
+    var counterValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterTitleLable.text = "Значение счётчика:"
+        counterLable.text = counterValue.description
+        addButtonStyle()
     }
 
-
+    @IBAction func addButtonPressed(_ sender: Any) {
+        counterValue += 1
+        counterLable.text = counterValue.description
+    }
+    
+    func addButtonStyle(){
+        addButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        addButton.backgroundColor = .clear
+        addButton.layer.cornerRadius = 5
+        addButton.layer.borderWidth = 1
+        addButton.layer.borderColor = UIColor.black.cgColor
+    }
 }
 
