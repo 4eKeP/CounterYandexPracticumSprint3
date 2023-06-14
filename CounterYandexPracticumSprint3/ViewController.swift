362c375f1972222abcf40a21ce23c41109906698
Egor <egor_chek@mail.ru>
2023-06-14 20:50:06 +0400

    Добавлена кнопка уменьшения значения счетчика в файлах ViewController и Main.

--- CounterYandexPracticumSprint3/ViewController.swift
+++ CounterYandexPracticumSprint3/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var decreaseButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
     @IBOutlet weak var counterLable: UILabel!
     @IBOutlet weak var counterTitleLable: UILabel!
@@ -20,16 +21,35 @@ class ViewController: UIViewController {
         counterTitleLable.text = "Значение счётчика:"
         counterLable.text = counterValue.description
         addButtonStyle()
+        decreaseButtonStyle()
     }
 
+    @IBAction func decreaseButtonPressed(_ sender: Any) {
+        if counterValue <= 0 {
+            counterValue = 0
+            counterLable.text = counterValue.description
+        }else{
+            counterValue -= 1
+            counterLable.text = counterValue.description
+        }
+    }
+    
     @IBAction func addButtonPressed(_ sender: Any) {
         counterValue += 1
         counterLable.text = counterValue.description
     }
     
+    func decreaseButtonStyle(){
+        decreaseButton.setImage(UIImage(systemName: "minus"), for: .normal)
+        decreaseButton.backgroundColor = .blue
+        decreaseButton.layer.cornerRadius = 5
+        decreaseButton.layer.borderWidth = 1
+        decreaseButton.layer.borderColor = UIColor.black.cgColor
+    }
+    
     func addButtonStyle(){
         addButton.setImage(UIImage(systemName: "plus"), for: .normal)
-        addButton.backgroundColor = .clear
+        addButton.backgroundColor = .red
         addButton.layer.cornerRadius = 5
         addButton.layer.borderWidth = 1
         addButton.layer.borderColor = UIColor.black.cgColor
