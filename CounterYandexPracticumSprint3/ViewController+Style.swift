330063f327ee91f8b224702b52dbdd76d03a3c1e
Egor <egor_chek@mail.ru>
2023-06-14 22:58:55 +0400

    функции для работы с интерфейсом выведены в отдельный файл ViewController+Style

---
+++ CounterYandexPracticumSprint3/ViewController+Style.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController+Style.swift
+//  CounterYandexPracticumSprint3
+//
+//  Created by admin on 14.06.2023.
+//
+
+import UIKit
+
+extension ViewController{
+    func historyTextViewStyle(){
+        historyTextView.layer.cornerRadius = 5
+        historyTextView.layer.borderWidth = 1
+        historyTextView.layer.borderColor = UIColor.black.cgColor
+        historyTextView.isEditable = false
+        historyTextView.text = "История изменений:\n"
+        historyTextView.textColor = .white
+        historyTextView.font?.withSize(17)
+    }
+    
+    func removeButtonStyle(){
+        removeButton.setImage(UIImage(systemName: "clear"), for: .normal)
+    }
+    
+    func decreaseButtonStyle(){
+        decreaseButton.setImage(UIImage(systemName: "minus"), for: .normal)
+        decreaseButton.backgroundColor = .blue
+        decreaseButton.layer.cornerRadius = 5
+        decreaseButton.layer.borderWidth = 1
+        decreaseButton.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func addButtonStyle(){
+        addButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        addButton.backgroundColor = .red
+        addButton.layer.cornerRadius = 5
+        addButton.layer.borderWidth = 1
+        addButton.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func counterTitleLableStyle(){
+        counterTitleLable.text = "Значение счётчика:"
+    }
+}
+
--- CounterYandexPracticumSprint3/ViewController.swift
+++ CounterYandexPracticumSprint3/ViewController.swift
@@ -58,41 +58,5 @@ class ViewController: UIViewController {
         historyTextView.text += "\(timeStamp.string(from: .now)): \(text).\n"
         historyTextView.scrollRangeToVisible(range)
     }
-    
-    func historyTextViewStyle(){
-        historyTextView.layer.cornerRadius = 5
-        historyTextView.layer.borderWidth = 1
-        historyTextView.layer.borderColor = UIColor.black.cgColor
-        historyTextView.isEditable = false
-        historyTextView.text = "История изменений:\n"
-        historyTextView.textColor = .white
-        historyTextView.font?.withSize(17)
-        
-        
-    }
-    
-    func removeButtonStyle(){
-        removeButton.setImage(UIImage(systemName: "clear"), for: .normal)
-    }
-    
-    func decreaseButtonStyle(){
-        decreaseButton.setImage(UIImage(systemName: "minus"), for: .normal)
-        decreaseButton.backgroundColor = .blue
-        decreaseButton.layer.cornerRadius = 5
-        decreaseButton.layer.borderWidth = 1
-        decreaseButton.layer.borderColor = UIColor.black.cgColor
-    }
-    
-    func addButtonStyle(){
-        addButton.setImage(UIImage(systemName: "plus"), for: .normal)
-        addButton.backgroundColor = .red
-        addButton.layer.cornerRadius = 5
-        addButton.layer.borderWidth = 1
-        addButton.layer.borderColor = UIColor.black.cgColor
-    }
-    
-    func counterTitleLableStyle(){
-        counterTitleLable.text = "Значение счётчика:"
-    }
 }
 
